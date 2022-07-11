37fc9f7672a9a6276c6c606c1042ef930554c37d
ramizarif <arifr@purdue.edu>
2022-07-11 03:29:25 -0400

    commit

--- XRBrainJamProject/Assets/Scripts/UiScripts/DestinationManager.cs
+++ XRBrainJamProject/Assets/Scripts/UiScripts/DestinationManager.cs
@@ -7,13 +7,23 @@ public class DestinationManager : MonoBehaviour
     [System.NonSerialized]
     public int numOfObjectsPutAway; 
 
+    [SerializeField]
+    GameObject[] soundTriggerObjects; 
+
+    [SerializeField]
+    AudioClip[] audioClipList; 
+
     //dictionary maps the tags of movable objects to the audio clip associated with the object
-    Dictionary<string, AudioClip> tagToAudioDictionary = new Dictionary<string, AudioClip>(); 
+    Dictionary<GameObject[], AudioClip> tagToAudioDictionary = new Dictionary<GameObject[], AudioClip>(); 
+
+    void Awake() {
+        
+    }
 
     void Update() {
         if (numOfObjectsPutAway > 3) {
             GetComponent<EventTransitioner>().endConditionReached = true; 
-            
+
         }
     }
 }
