905dd414acee57616be321060f17ee42933c3b1b
ramizarif <arifr@purdue.edu>
2022-07-12 19:55:17 -0400

    prebuild
    fixed:
    - turning off phone in the set
    - phone raycast distance is unlimited
    - collision detection instead of vector distance destination system

--- XRBrainJamProject/Assets/Scripts/PhoneEventBehavior.cs
+++ XRBrainJamProject/Assets/Scripts/PhoneEventBehavior.cs
@@ -29,36 +29,16 @@ public class PhoneEventBehavior : MonoBehaviour
         phoneDist = Vector3.Distance(arCamera.transform.position, phone.transform.position); 
 
         if (Input.touchCount > 0) {
-            if (phoneDist < 1) {
-                RaycastHit hit; 
-                Ray ray = arCamera.ScreenPointToRay(Input.GetTouch(0).position);
-                if (Physics.Raycast(ray, out hit)) {
-                    if (hit.collider.gameObject == phone) {
-                        Debug.Log("hit the phone baby!!");  
-                        phoneCanvas.SetActive(true);
-                        gameObject.SetActive(false); 
-                        GetComponent<EventTransitioner>().endConditionReached = true; 
-                    }
+            RaycastHit hit; 
+            Ray ray = arCamera.ScreenPointToRay(Input.GetTouch(0).position);
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.collider.gameObject == phone) {
+                    phoneCanvas.SetActive(true);
+                    gameObject.SetActive(false); 
+                    phone.SetActive(false); 
+                    GetComponent<EventTransitioner>().endConditionReached = true; 
                 }
             }
         }
-
-        if (phoneDist <  1f) {
-            
-        }
-
-        // var phoneDist = Vector3.Distance(arCamera.transform.position, phone.transform.position); 
-        // if (phoneDist > 3) {
-        //     alpha = 1; 
-        //     introPanelImage.color = new Color(0, 0, 0, alpha); 
-        //     panelReadyImage.color = new Color(0, 0, 0, alpha); 
-        // } else if (phoneDist < 2f) {
-        //     panelReady.GetComponent<EventTransitioner>().endConditionReached = true; 
-        //     phone.SetActive(false);
-        // }  else if (phoneDist < 3 && phoneDist > 0) {
-        //     alpha = 1 - (1 / phoneDist / 6); 
-        //     introPanelImage.color = new Color(0, 0, 0, alpha); 
-        //     panelReadyImage.color = new Color(0, 0, 0, alpha); 
-        // }
     }
 }
