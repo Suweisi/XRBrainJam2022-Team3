676a6636ad7edd5f7fa04688dd4e59d30cf712e5
ramizarif <arifr@purdue.edu>
2022-07-13 15:04:48 -0400

    fixes

--- XRBrainJamProject/Assets/Scripts/UiScripts/DebriefOneBehavior.cs
+++ XRBrainJamProject/Assets/Scripts/UiScripts/DebriefOneBehavior.cs
@@ -3,7 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DebriefOneBehavior : MonoBehaviour
-{
+{   
+
+    [SerializeField]
+    GameObject setObj; 
+
+    void Awake() {
+        setObj.SetActive(false); 
+    }
+
      public void PressNote() {
         GetComponent<EventTransitioner>().endConditionReached = true; 
         gameObject.SetActive(false); 
